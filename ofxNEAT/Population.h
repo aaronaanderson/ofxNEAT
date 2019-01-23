6a9199a437528bd4098473bd275e304b5ddc5062
aaronaanderson <aaron.arthur.anderson@gmail.com>
2019-01-23 02:52:49 -0800

    convering drafts to code

--- ofxNEAT/Population.h
+++ ofxNEAT/Population.h
@@ -1,9 +1,29 @@
 #pragma once
 
+#include "ofMain.h"
+#include "Specie.h"
+
 class Population {
 
 public:
 
+	Population(unsigned int numInputs, unsigned int numOutputs) {
+		Specie temp(numInputs, numOutputs);
+		species.push_back(temp);
+	}
+
+	unsigned int fittestGenome(){
+		float highestFitness = 0;
+		for (int i; i < species.size(); i++) {
+			highestFitness = fmax(highestFitness, species[i].getFittest())
+		}
+	}
+
 private:
 
+	vector<Specie> species;
+	unsigned int numSpecies;
+	bool checkForSpeciation;
+	unsigned long globalInovationNumber;
+	unsigned int mostFitGenome;
 };
--- ofxNEAT/Specie.h
+++ ofxNEAT/Specie.h
@@ -1,10 +1,22 @@
 #pragma once
 
+#include "ofMain.h"
+#include "Genome.h"
 
 class Specie {
 
 public:
 
+	unsigned int getFittest() {
+		float highestFitnes = 0;
+		for (int i = 0; i < genomes.size(); i++) {
+			highestFitnes = fmax(highestFitnes, genomes[i].getFitness());
+		}
+	}
 private:
+	vector<Genome> genomes;
+	unsigned int numGenomes;
+	Genome performCrossover(Genome a, Genome b);
+	bool checkForSpeciation(Genome a, Genome* species);
 
 };
