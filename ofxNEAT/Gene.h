25da962aa60a5ee2b2f35f4f3e36c42404136bd2
aaronaanderson <aaron.arthur.anderson@gmail.com>
2019-01-23 03:07:44 -0800

    more conversion from draft

--- ofxNEAT/Gene.h
+++ ofxNEAT/Gene.h
@@ -1,9 +1,23 @@
 #pragma once
 
+struct Connection {
+	unsigned int outputNode;
+	float weight;
+	bool enabled;
+};
+
+//the terms Gene and node are interchangeable in this context
 class Gene {
 
 public:
 
 private:
+	float nodeValue;
+	float bias;
+	unsigned int nodeIdentity;
+	unsigned int localAge;
+	bool enabled;//I don't think this is needed (DELETE)
+	vector<Connection> outwardConnections;
+	vector<unsigned int> inwardConnections;
 
 };
--- ofxNEAT/Genome.h
+++ ofxNEAT/Genome.h
@@ -1,9 +1,19 @@
 #pragma once
 
+#include "ofMain.h"
+#include "Gene.h"
+
 class Genome {
 
 public:
 
 private:
+	void createInitialGenome(unsigned int numInputs, unsigned int numOutputs);
+	float checkFitness();
+	float* calculateOutputs(float* inputs);
+	void drawGenome();
 
+	vector<Gene> genes;
+	unsigned int numLayers = 2;
+	vector<unsigned int> numNodesInLayer;
 };
